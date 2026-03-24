0ec5a5db09cb57a3d26358c934d2c64dfab23bcc
Otito Amuga <urphasin@gmail.com>
2026-03-23 18:11:22 -0600

    fgfg

--- dsa/areAnagrams.py
+++ dsa/areAnagrams.py
@@ -2,12 +2,24 @@ def areAnagrams(str1: str, str2: str) -> bool:
     if len(str1) != len(str2):
         return False
     
-    freqStr1 = {}
-    freqStr2 = {}
+    freq1 = freqeuncy1(str1)
+    freq2 = frequency2(str2)
 
-    for s in str1:
-        if s not in freqStr1:
-            freqStr1[s] = 1        
 
-    for keys, values in freqStr1.items():
-        
+
+def freqeuncy1(S: str) -> dict:
+    freq = {}
+    for s in S:
+        if s not in freq:
+            freq[s] = 0
+        freq[s] += 1
+    return freq
+
+def frequency2(S: str) -> dict:
+    freq = {}
+    for s in freq:
+        freq.setdefault(s, 0)
+        freq[s] += 1
+    return freq
+
+areAnagrams("otito", "ttooi")
